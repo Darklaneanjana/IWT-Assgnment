a21f1c831c91833f9286245b0968223b682391f2
kalana_sithum <kalanasithum1211@gmail.com>
2021-05-25 16:21:35 +0530

    developer

--- devAccount.php
+++ devAccount.php
@@ -1,31 +1,40 @@
 <link rel="stylesheet" type="text/css" href="css/devaccount.css?v=1" />
 <?php
-    require_once('inc/dev.inc.php');
-    require_once('php/header.php');
+require_once('php/header.php');
 
-    if (isset($_SESSION["userID"])){
-        if ($_SESSION["type"]!=="Dev"){
-            header("location:index.php?error=notDev");
-            exit();
-        }else{
-            echo "<p>Hello there ".$_SESSION["name"]."</p>";
-        }
-        
-    }else{
-        header("location:signin.php?error=notLoggedIn");
-            exit();
+if (isset($_SESSION["userID"])) {
+    if ($_SESSION["type"] !== "Dev") {
+        header("location:index.php?error=notDev");
+        exit();
+    } else {
+        echo "<p>Hello there " . $_SESSION["name"] . "</p>";
     }
+} else {
+    header("location:signin.php?error=notLoggedIn");
+    exit();
+}
 ?>
 
+<body>
+
+    <form action="inc/dev.inc.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="appName" placeholder="App Name"><br>
+        <input type="text" name="Description" placeholder="App Description"><br>
+        <input type="text" name="appType" placeholder="App Type"><br>
+        <input type="text" name="buildNo" placeholder="BuildNo"><br>
+        <input type="text" name="appCat" placeholder="App Category"><br>
+        <input type="file" name="appSS"><br>
+        <input type="file" name="appFile"><br>
+        <input type="submit" name="submit" value="Upload">
+        <a href="devAccount.php">Cancel</a>
+
+    </form>
 
 
 
 
-
-
-
-
+</body>
 
 <?php
 require_once('php/footer.php');
-?> 
+?>
--- inc/dev.inc.php
+++ inc/dev.inc.php
@@ -1,25 +1,66 @@
 <?php
-if (isset($_POST['submit'])) {
-    $file = $_FILES['SS'];
+require_once 'dbh.inc.php';
 
-    $fileName = $_FILES['SS']['name'];
-    $fileTmpName = $_FILES['SS']['tmp_name'];
-    $fileSize = $_FILES['SS']['size'];
-    $fileError = $_FILES['SS']['error'];
-    $fileType = $_FILES['SS']['type'];
+
+
+if (isset($_POST['submit'])) {
+
+    /*$appName = $_POST['appName'];
+    $Description = $_POST['Description'];
+    $appType = $_POST['appType'];
+    $buildNo = $_POST['buildNo'];
+    $appCat = $_POST['appCat'];*/
+
+    $appSS = $_FILES['appSS'];
+
+    $appSSname = $_FILES['appSS']['name'];
+    $appSStmpName = $_FILES['appSS']['tmp_name'];
+    $appSSsize = $_FILES['appSS']['size'];
+    $appSSError = $_FILES['appSS']['error'];
+    $appSSType = $_FILES['appSS']['type'];
+
+    $appSSExt = explode('.', $appSSname);
+    $appSSActualExt = strtolower(end($appSSExt));
+
+    $allowedSS = array('jpg', 'jpeg', 'png', 'gif');
+
+    if (in_array($appSSActualExt, $allowedSS)) {
+        if ($appSSError === 0) {
+            if ($appSSsize < 50000000) {
+                $appSSnewName = uniqid('', true) . '.' . $appSSActualExt;
+                $appSSDestination = '../APPSS/' . $appSSnewName;
+                move_uploaded_file($appSStmpName, $appSSDestination);
+                // header("Location: ../devAccount.php?uploadsuccessfull");
+            } else {
+                echo "Your file is too big..!!!";
+            }
+        } else {
+            echo "There was an error uploading your file..!!!";
+        }
+    } else {
+        echo "You cannot upload of files this type..!!!";
+    }
+
+    $file = $_FILES['appFile'];
+
+    $fileName = $_FILES['appFile']['name'];
+    $fileTmpName = $_FILES['appFile']['tmp_name'];
+    $fileSize = $_FILES['appFile']['size'];
+    $fileError = $_FILES['appFile']['error'];
+    $fileType = $_FILES['appFile']['type'];
 
     $fileExt = explode('.', $fileName);
     $fileActualExt = strtolower(end($fileExt));
 
-    $allowed = array('jpg', 'jpeg', 'png', 'gif');
+    $allowed = array('apk', 'xapk', 'apks', 'apkm');
 
     if (in_array($fileActualExt, $allowed)) {
         if ($fileError === 0) {
-            if ($fileSize < 500000) {
-                $fileNewName = uniqid('', true) . '0' . $fileActualExt;
-                $fileDestination = '../APP';
+            if ($fileSize < 50000000) {
+                $fileNewName = uniqid('', true) . '.' . $fileActualExt;
+                $fileDestination = '../APP/' . $fileNewName;
                 move_uploaded_file($fileTmpName, $fileDestination);
-                header("Location : dev.inc.php?uploadsuccess");
+                header("Location: ../devAccount.php?uploadsuccessfull");
             } else {
                 echo "Your file is too big..!!!";
             }
@@ -30,3 +71,5 @@ if (isset($_POST['submit'])) {
         echo "You cannot upload of files this type..!!!";
     }
 }
+//$sql = "INSERT INTO app(appID, appName, Description, price, appType, buildNo, devID, catID, size, appCat)
+//VALUES('','$appName','$Description','$price','$appType','$buildNo','','','$fileSize','$appCat');";
