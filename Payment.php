9df0e391d2ec253994fa5ae08f434071def6ce01
Darklaneanjana <darklaneanjana@gmail.com>
2021-06-16 21:38:15 +0530

    ddd

--- Payment.php
+++ Payment.php
@@ -9,6 +9,7 @@
 
 <?php
 require_once('php/header.php');
+require_once('inc/dbh.inc.php.php');
 
 if (isset($_SESSION["userID"])) {
     if ($_SESSION["type"] !== "User") {
@@ -24,25 +25,70 @@ if (isset($_SESSION["userID"])) {
 ?>
 
 
+
+
+
+
 <?php
-    echo $_GET["app"].'<br>' ; 
-    echo $_SESSION["uid"].'<br>';
-    echo $_SESSION["userID"].'<br>';
-    echo $_SESSION["name"];
+if(isset($_POST['submit'])){
+
+$card = $_POST['cardnum'];
+$exp = $_POST['expDate'];
+$cvv = $_POST['cvvNum'];
+$exp = $_POST['Exyear'];
+
+
+$query ="INSERT INTO payment(card_number,exp_month,cvv,exp_year)VALUES('{$card}','{$exp}','{$cvv}','{$exp}')";
+
+
+$result_set = mysqli_query($connection, $query);
+
+
+if($result_set){
+	echo"add";
+}else{
+	echo"err";
+
+
+}
+}
 ?>
 
-<body>
-    <h2>Payment</h2>
+
+
+ 
+   <body>
+   <center>
+    <h1>Payment</h1><br>
     <h3>Accepted Cards</h3>
-    <form action="process.php" method="POST">
-     <lable>credit card number</lable>
+
+
+
+    <form action="Payment.php" method="post">
+	
+     <label>Card Number</label>
     <input type="text" name="cardnum">
+    <br><br>
     <label>EXP Month</label> 
     <input type="text"name="expDate">
+    <br><br>
+    <label>CVV</label>
+   <input type="text" name="cvvNum">
+   <br><br>
+    <label>EXP Year</label>
+    <input type="text" name="Exyear">
+    <br><br>
 
-
+	<input type = "submit" value ="~-submit-~" name="submit">
     </form>
-</body>
+</center>
+
+
+
+
 <?php
+
 require_once('php/footer.php');
 ?> 
+</body>
+</html>
